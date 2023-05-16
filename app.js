4e545b34d0c64239924c4564cfd0f5928aaedf31
shubhamcompro23 <shubham.saini@comprotechnologies.com>
2023-05-16 19:22:16 +0530

    redis problem solution

---
+++ app.js
@@ -0,0 +1,61 @@
+const redisController = require("./redis")
+
+
+async function data() {
+
+    //Expiry Logger 
+    await redisController.expireLogger()
+
+    // ADD NEW KEY
+    let addkeyoptions = {
+        keyName: "newkey",
+        expiry: 60,
+        value: "This is value of new key"
+    }
+    const addkey = await redisController.addNewKey(addkeyoptions)
+
+    console.log("addnewkey--", addkey)
+
+    //REMOVE EXPIRY
+
+    let removeexpiryoptions= {
+        key : "newkey"
+    }
+
+    const removeExpiry = await redisController.removeExpiry(removeexpiryoptions)
+
+    console.log("removeExpiry--", removeExpiry)
+
+    //updateExpiry
+
+    let updateExpiryoptions = {
+        key: "newkey",
+        expiry: 40
+    }
+    const updateExpiry = await redisController.updateExpiry(updateExpiryoptions)
+
+    console.log("updateExpiry--", updateExpiry)
+
+    //getKey
+
+    let getoptions = {
+        key: "newkey"
+    }
+
+    const getKey = await redisController.getKey(getoptions)
+
+    console.log("getKey--", getKey)
+
+    //updatevalue
+
+    const updatevalueoptions = {
+        key: "newkey",
+        value: "This is updated value of new key"
+    }
+
+    const updatedValue = await redisController.updateValue(updatevalueoptions)
+
+    console.log("updatedValue",updatedValue)
+}
+
+data()
---
+++ redis.js
@@ -0,0 +1,132 @@
+const { createClient } = require('redis');
+const dotenc = require("dotenv").config()
+const port = 6379
+const client = createClient(port);
+client.connect();
+client.on('connect', () => console.log('connect to redis server'));
+
+
+
+//Support for creating a new key of type string in the Database.
+//Provide support for, optionally, adding an expiry on the key.
+
+
+
+async function addNewKey(options){
+
+    try{
+        const polutedKey = process.env.POLLUTANT + ":" + options.keyName
+        const polluteData = await client.set(polutedKey, options.value)
+        if(options.expiry){
+            const data = await client.setEx(options.keyName, options.expiry , options.value)
+            return {data, polluteData}
+        }else{
+            const data = await client.set(polutedKey, options.value)
+            return {data, polluteData}
+        }
+        
+    }catch(err){
+        return err
+    }
+
+} 
+
+// Support for removing the expiry of any existing key in the Database.
+
+async function removeExpiry(options) {
+    try{
+        const value = await client.get(options.key)
+        if(value){
+            const ttl = await client.ttl(options.key)
+
+            if(ttl){
+                let data = await client.persist(options.key)
+                return data
+            }else{
+                return new Error("Key already persist")
+            }
+        }else{
+            return new Error("Key does not exist")
+        }
+    }catch(err){
+        //console.log("error---", err)
+        return err
+    }
+}
+
+// Support for adding/updating the expiry on any existing key in the Database.
+
+async function updateExpiry(options){
+    try{
+        const data = await client.expire(options.key,options.expiry)
+
+        if(!data){
+            console.log("error---", new Error("Key does not exist...!"))
+        }
+
+        return data
+
+    }catch(err){
+        return err
+    }
+}
+
+
+//Support for fetching the value of any existing key in the Database.
+
+async function getKey(options) {
+    
+    try{
+        const data = await client.get(options.key)
+        if(data){
+            return data
+        }
+        return new Error("Key does not exist")
+    }catch(err){
+        return err
+    }
+}
+
+// Support for logging the name & value of any key upon the keys' expiration.
+
+
+async function expireLogger () {
+  await client.configSet("notify-keyspace-events", "KEx");
+
+  const subscriber=client.duplicate();
+  subscriber.connect();
+
+
+  await subscriber.SUBSCRIBE("__keyevent@0__:expired", async(message) => {
+    const data = await client.get(`${process.env.POLLUTANT}:${message}`)
+    console.log("Logger",{
+        expireKey: message,
+        value: data
+    }) 
+    
+  });
+
+}
+
+async function updateValue(options) {
+    
+    try{
+        const data = await client.GETSET(options.key,options.value)
+        const polutekeyvalue = await client.GETSET(process.env.POLLUTANT + ":" + options.key,options.value)
+        if(data){
+            return {keyValue: data, pollutekeyValue: polutekeyvalue}
+        }
+        return new Error("Key does not exist")
+    }catch(err){
+        return err
+    }
+}
+
+module.exports= {
+    addNewKey,
+    removeExpiry,
+    updateExpiry,
+    getKey,
+    expireLogger,
+    updateValue
+}
